a23e958bd77d5c837aa7524c3acd5e0a84c244ec
tanggen911 <tanggen@hfhj178.com>
2026-05-26 13:46:12 +0800

    fix bug

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -254,6 +254,10 @@ export function put(url: string, data = {}) {
   return instance.put(url, data);
 }
 
+export function patch(url: string, data = {}) {
+  return instance.patch(url, data);
+}
+
 export function del(url: string, data?: any) {
   return instance.delete(url, { data });
 }
